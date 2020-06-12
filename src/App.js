6f7f2612cdda27cf0a52e8d307b737f566e4b656
abdulbasit <abbasi097@gmail.com>
2020-06-12 16:01:59 +0400

    tutorial Task 1
    React tutorial Task 1 Completed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './task1.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          This file is updated by abdul basit from home PC
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (<div className="main">
+    Hello World from <strong>Abdul Basit</strong>
+    <h3>Courses Offered By PIAIC</h3>
+    <ul>
+        <li>BCC</li>
+        <li>CNC</li>
+        <li>IOT</li>
+        <li>AIC</li>
+    </ul>
+    <br/>
+    <h3>
+        My Favourite Sports
+    </h3>
+    <ol className="list">
+        <li>Cricket</li>
+        <li>Foot Ball</li>
+        <li>Table Tennis</li>
+    </ol>
+    <br/>
+    <p>
+        This sum is creted using JSX in react 5 + 5 =<i> {5+5} </i>
+    </p>
+</div>)
 
 export default App;
