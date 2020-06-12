8082177c3a49c69a39ac80a9481418baf2211d48
abdulbasit <abbasi097@gmail.com>
2020-06-12 11:34:01 +0400

    Made updates from Home PC

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          This file is updated by abdul basit from home PC
         </p>
         <a
           className="App-link"
